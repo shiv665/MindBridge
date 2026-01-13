82c5d874fbaa0ebc580b558a57aa9e299a72a912
SHIVANSH YADAV <shivanshsdp@gmail.com>
2026-01-13 22:24:43 +0530

    API UPDATION

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
